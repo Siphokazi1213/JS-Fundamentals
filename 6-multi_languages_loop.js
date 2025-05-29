52fd279b9b6202d9cea9010da4975c236b0d7f60
Sipokazi Nyathi <siphokazinyathi@gmail.com>
2025-05-29 11:20:07 +0200

    feat: Add looped multi-language script with array and single log

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+let outputString = "";
+
+for (const phrase of languages) {
+  outputString += phrase + "\n";
+}
+console.log(outputString);
